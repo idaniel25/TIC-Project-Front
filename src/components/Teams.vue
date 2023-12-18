327edb666dc7336c8bccb53e7a96e33915394f3d
danutz <iovanadaniel3@gmail.com>
2023-12-18 13:54:41 +0200

    implement update team/player functionality

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -11,6 +11,29 @@
         </form>
 
       <h2>Echipe și Jucători</h2>
+
+      <!-- Adaugă formularul de editare a echipei -->
+      <div v-if="editedTeam.id !== ''">
+            <h3>Editare echipă</h3>
+            <label for="editedTeamName">Nume echipă:</label>
+            <input v-model="editedTeam.name" id="editedTeamName" />
+            <button @click="saveEditedTeam">Salvează</button>
+      </div>
+
+      <!-- Adaugă formularul de editare a jucătorului -->
+      <div v-if="editedPlayer.id !== ''">
+        <h3>Editare jucător</h3>
+        <label for="editedPlayerName">Nume jucător:</label>
+        <input v-model="editedPlayer.name" id="editedPlayerName" />
+        
+        <!-- Adaugă selectorul pentru echipă -->
+        <label for="editedPlayerTeam">Echipă:</label>
+        <select v-model="editedPlayer.team_id" id="editedPlayerTeam">
+          <option v-for="team in teams" :key="team.id" :value="team.id">{{ team.name }}</option>
+        </select>
+
+        <button @click="saveEditedPlayer">Salvează</button>
+      </div>
   
       <!-- Lista de echipe și jucători -->
       <ul>
@@ -36,6 +59,7 @@
           </ul>
         </li>
       </ul>
+      
     </div>
   </template>
   
@@ -46,6 +70,8 @@
     data() {
       return {
         teams: [],
+        editedTeam: { id: '', name: '' }, // Noua proprietate pentru a ține detaliile echipei editate
+        editedPlayer: { id: '', name: '', team_id: '' }, // Noua proprietate pentru a ține detaliile jucătorului editat
       };
     },
     mounted() {
@@ -77,19 +103,28 @@
         console.error('Eroare la adăugarea echipei:', error);
       }
     },
-    async addPlayer(team) {
-      try {
-        await axios.post('http://localhost:3000/players', { name: this.newPlayerName, team_id: team.id });
-        this.newPlayerName = ''; // Resetează câmpul de intrare
-        this.loadData(); // Actualizează lista de echipe și jucători
-      } catch (error) {
-        console.error('Eroare la adăugarea jucătorului:', error);
-      }
-    },
+      async addPlayer(team) {
+        try {
+          await axios.post('http://localhost:3000/players', { name: this.newPlayerName, team_id: team.id });
+          this.newPlayerName = ''; // Resetează câmpul de intrare
+          this.loadData(); // Actualizează lista de echipe și jucători
+        } catch (error) {
+          console.error('Eroare la adăugarea jucătorului:', error);
+        }
+      },
       async editTeam(team) {
+        // Setează detaliile echipei editate
+        this.editedTeam = { id: team.id, name: team.name };
+      },
+      async saveEditedTeam() {
         // Implementează funcționalitatea de editare pentru echipă (PUT sau PATCH către API)
         try {
-          const response = await axios.put(`http://localhost:3000/echipe/${team.id}`, team);
+          const response = await axios.put(`http://localhost:3000/teams/${this.editedTeam.id}`, { name: this.editedTeam.name });
+          // După actualizare, încarcă din nou echipele și jucătorii pentru a reflecta schimbările
+          this.loadData();
+
+          // Resetează detaliile echipei editate
+          this.editedTeam = { id: '', name: '' };
           console.log('Echipa actualizată cu succes:', response.data);
         } catch (error) {
           console.error('Eroare la actualizarea echipei:', error.message);
@@ -106,9 +141,21 @@
         }
       },
       async editPlayer(player) {
+        // Setează detaliile jucătorului editat
+        this.editedPlayer = { id: player.id, name: player.name, team_id: player.team_id };
+      },
+      async saveEditedPlayer() {
         // Implementează funcționalitatea de editare pentru jucător (PUT sau PATCH către API)
         try {
-          const response = await axios.put(`http://localhost:3000/jucatori/${player.id}`, player);
+          const response = await axios.put(`http://localhost:3000/players/${this.editedPlayer.id}`, {
+          name: this.editedPlayer.name,
+          team_id: this.editedPlayer.team_id,
+        });
+           // După actualizare, încarcă din nou echipele și jucătorii pentru a reflecta schimbările
+          this.loadData();
+
+          // Resetează detaliile jucătorului editat
+          this.editedPlayer = { id: '', name: '', team_id: '' };
           console.log('Jucător actualizat cu succes:', response.data);
         } catch (error) {
           console.error('Eroare la actualizarea jucătorului:', error.message);
