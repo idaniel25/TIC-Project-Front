64087def7329e90058339f61f0b03b37ddc0178c
danutz <iovanadaniel3@gmail.com>
2023-12-18 15:35:48 +0200

    improve design

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -2,6 +2,7 @@
 
 <template>
     <div>
+      <div class="add-form">
         <!-- Formular pentru adăugarea unei echipe -->
         <form @submit.prevent="addTeam">
           <h3>Adaugă echipă</h3>
@@ -9,8 +10,15 @@
           <input type="text" v-model="newTeamName" id="teamName" required />
           <button type="submit">Adaugă echipă</button>
         </form>
-
-      <h2>Echipe și Jucători</h2>
+  
+        <!-- Formular pentru adăugarea unui jucător la echipă -->
+        <form v-if="isAddPlayerFormVisible" @submit.prevent="saveAddedPlayer">
+          <h4>Adaugă jucător</h4>
+          <label for="playerName">Nume jucător:</label>
+          <input type="text" v-model="newPlayerName" id="playerName" required />
+          <button type="submit">Adaugă jucător</button>
+        </form>
+      </div>
 
       <!-- Adaugă formularul de editare a echipei -->
       <div v-if="editedTeam.id !== ''">
@@ -34,35 +42,61 @@
 
         <button @click="saveEditedPlayer">Salvează</button>
       </div>
-  
+
+      <h2>Echipe și Jucători</h2>
+
       <!-- Lista de echipe și jucători -->
-      <ul>
-        <li v-for="team in teams" :key="team.id">
-          {{ team.name }}
-          <button @click="editTeam(team)">Edit</button>
-          <button @click="deleteTeam(team)">Delete</button>
+      <div>
+        <div v-for="team in teams" :key="team.id" class="team-container">
+          <div class="team-header">
+            {{ team.name }}
+            <button @click="editTeam(team)">Edit</button>
+            <button @click="deleteTeam(team)">Delete</button>
+            <button @click="showAddPlayerForm(team.id)">Add a player</button>
+          </div>
   
-          <!-- Formular pentru adăugarea unui jucător la echipă -->
-          <form @submit.prevent="addPlayer(team)">
-            <h4>Adaugă jucător</h4>
-            <label for="playerName">Nume jucător:</label>
-            <input type="text" v-model="newPlayerName" id="playerName" required />
-            <button type="submit">Adaugă jucător</button>
-          </form>
-  
-          <ul>
-            <li v-for="player in team.players" :key="player.id">
+          <ul class="player-list">
+            <li v-for="player in team.players" :key="player.id" class="player-item">
               {{ player.name }}
               <button @click="editPlayer(player)">Edit</button>
               <button @click="deletePlayer(player)">Delete</button>
             </li>
           </ul>
-        </li>
-      </ul>
+        </div>
+      </div>
       
     </div>
   </template>
   
+<style scoped>
+    
+    .team-container {
+      margin: auto;
+      margin-bottom: 20px;
+      border: 10px solid #ddd; /* Adaugă un border între echipe */
+      padding: 10px;
+      max-width: 400px;
+    }
+    
+    .team-header {
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+    
+    .player-list {
+      margin: auto;
+      list-style: none; /* Elimină stilul de marcă de listă */
+      padding: 0; /* Elimină spațiul de margini între liste */
+      width: 200px;
+    }
+    
+    .player-item {
+      border: 1px solid #eee; /* Adaugă un border între jucători */
+      padding: 5px;
+      margin-bottom: 5px;
+    }
+</style>
+
   <script>
   import axios from 'axios';
   
@@ -72,6 +106,8 @@
         teams: [],
         editedTeam: { id: '', name: '' }, // Noua proprietate pentru a ține detaliile echipei editate
         editedPlayer: { id: '', name: '', team_id: '' }, // Noua proprietate pentru a ține detaliile jucătorului editat
+        isAddPlayerFormVisible: false,
+        selectedTeamId: null,
       };
     },
     mounted() {
@@ -103,10 +139,17 @@
         console.error('Eroare la adăugarea echipei:', error);
       }
     },
-      async addPlayer(team) {
+      showAddPlayerForm(teamId) {
+        this.isAddPlayerFormVisible = true;
+        this.selectedTeamId = teamId;
+      },
+      async saveAddedPlayer() {
         try {
-          await axios.post('http://localhost:3000/players', { name: this.newPlayerName, team_id: team.id });
+          const selectedTeam = this.teams.find((t) => t.id === this.selectedTeamId); // Adaugă această linie
+
+          await axios.post('http://localhost:3000/players', { name: this.newPlayerName, team_id: selectedTeam.id });
           this.newPlayerName = ''; // Resetează câmpul de intrare
+          this.isAddPlayerFormVisible = false; // Ascunde formularul după adăugarea jucătorului
           this.loadData(); // Actualizează lista de echipe și jucători
         } catch (error) {
           console.error('Eroare la adăugarea jucătorului:', error);
